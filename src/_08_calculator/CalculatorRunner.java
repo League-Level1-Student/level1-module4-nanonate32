25b78b3565e2ebd15e9f08602a620424856deddb
league <league@imac-10.attlocal.net>
2019-12-29 12:55:55 -0800

    calculator CalculatorRunner

--- src/_08_calculator/CalculatorRunner.java
+++ src/_08_calculator/CalculatorRunner.java
@@ -1,10 +1,14 @@
 package _08_calculator;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class CalculatorRunner {
 
 	public static void main(String[] args) {
 	calculator calculator = new calculator();
-       add()
+     calculator.run();
 	}
 
 }
--- src/_08_calculator/calculator.java
+++ src/_08_calculator/calculator.java
@@ -1,6 +1,39 @@
 package _08_calculator;
 
-public class calculator {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+ 
+public class calculator implements ActionListener {JFrame frame = new JFrame();
+	      JPanel panel = new JPanel();
+	      JButton add = new JButton(add);
+	      JButton subtract = new JButton(subtract);	
+	      JButton multiply = new JButton(multiply);	
+	      JButton divide = new JButton(divide);   
+	      JTextField firstInt = new JTextField(10);
+	      JTextField secondInt = new JTextField(10);
+	void run() {	
+		frame.setVisible(true);
+		frame.add(panel);
+		 panel.add(add);
+	      panel.add(subtract);
+	      panel.add(multiply);
+	      panel.add(divide);
+	      panel.add(firstInt);
+	      panel.add(secondInt);
+	      add.addActionListener(this);
+	      subtract.addActionListener(this);
+	      multiply.addActionListener(this);
+	      divide.addActionListener(this);
+	      frame.pack();
+	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	     
+	}
+	
 int add(int firstNum, int secondNum) {
 	return firstNum+ secondNum;
 	
@@ -15,5 +48,29 @@ int divide(int firstNum, int secondNum) {
 	return firstNum/ secondNum;
 }
 
+@Override        
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	  int num = Integer.parseInt(firstInt.getText());
+	  int num2 = Integer.parseInt(firstInt.getText());
+	if(e.getSource() == add) {
+		add(num, num2);
+		
+	}
+	if(e.getSource() == subtract) {
+		subtract(num, num2);
+		
+	}
+	if(e.getSource() ==  multiply) {
+		multiply(num, num2);
+		
+	}
+	if(e.getSource() == divide) {
+		divide(num, num2);
+		
+	}
+	}
 }
+
+
 
