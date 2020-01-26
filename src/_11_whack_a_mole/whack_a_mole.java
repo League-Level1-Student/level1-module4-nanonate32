40e6f42b81b713cce45181cb150c34e87e168d6a
league <league@imac-13.attlocal.net>
2020-01-25 17:25:25 -0800

    whack_a_mole

--- src/_11_whack_a_mole/whack_a_mole.java
+++ src/_11_whack_a_mole/whack_a_mole.java
@@ -1,28 +1,62 @@
 package _11_whack_a_mole;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class whack_a_mole {
-JFrame frame = new JFrame();
-JPanel panel = new JPanel();	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
-JButton button = new JButton();
+public class whack_a_mole implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
 
-}
+	void run() {
+		frame.add(panel);
+		GridLayout layout = new GridLayout(8, 3);
+		panel.setLayout(layout);
+		makeButtons();
+		Random random = new Random();
+		drawButtons(random.nextInt(24));
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	void drawButtons(int randy) {
+		
+   JButton button = (JButton) panel.getComponent(randy);
+    button.setText("mole!");
+	}
+	void speak(String words) { 
+	    try { 
+	        Runtime.getRuntime().exec("say " + words).waitFor();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	void makeButtons() {
+		for (int i = 0; i < 24; i++) {
+			JButton button = new JButton();
+			button.addActionListener(this);
+			panel.add(button);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		whack_a_mole bob = new whack_a_mole();
+		bob.run();
+		
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() )
+	}
+
 }
