7b560d8ccf283b9305bb2a5d79e3419250207fe0
wkwak2021 <Francis2021>
2020-02-01 17:24:31 -0800

    whack_a_Mole

--- src/_11_whack_a_mole/whack_a_mole.java
+++ src/_11_whack_a_mole/whack_a_mole.java
@@ -3,17 +3,28 @@ package _11_whack_a_mole;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class whack_a_mole implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-
+	
+	
+    
+    
+    int molesWhacked = 0;
+    int molesMissed = 0;
+    Date TimeAtStart;
+    
 	void run() {
+		TimeAtStart = new Date(); 
 		frame.add(panel);
 		GridLayout layout = new GridLayout(8, 3);
 		panel.setLayout(layout);
@@ -43,8 +54,14 @@ public class whack_a_mole implements ActionListener {
 			button.addActionListener(this);
 			panel.add(button);
 		}
+		
 	}
-
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+	    Date timeAtEnd = new Date();
+	    JOptionPane.showMessageDialog(null, "Your whack rate is "
+	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	                  + " moles per second.");
+	}
 	public static void main(String[] args) {
 
 		whack_a_mole bob = new whack_a_mole();
@@ -52,11 +69,29 @@ public class whack_a_mole implements ActionListener {
 		
 
 	}
+	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource() )
+		if(((JButton) e.getSource()).getText().equals("mole!")) {
+		speak("correct");
+		 molesWhacked +=1;
+		 
+		 
+		}
+		else {
+		speak("mist");
+		molesMissed +=1;
+		
+		}
+		if(molesMissed == 5) {
+			JOptionPane.showMessageDialog(null, "You lose!");
+			endGame(TimeAtStart,molesWhacked);
+		}
+		frame.dispose();
+		makeButtons();
+		}
 	}
 
-}
+
